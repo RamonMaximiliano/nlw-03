979b1a8f1d1f74d76becf6421997f26505c5b778
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2022-09-13 23:34:31 -0300

    nice

---
+++ scripts/hope-script.js
@@ -0,0 +1,25 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+
+
+/* var map = L.map('map').setView([51.505, -0.09], 15);
+
+var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('<b>Hello world!</b><br />I am a popup.').openPopup();
+
+var circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map).bindPopup('I am a circle.'); */
